30a54c4d01e1df1d79089a90a467520368d12508
Valentin Dufois <valentin@dufois.fr>
2019-08-20 16:30:29 -0400

    Adding convenient access to BroadSign properties

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 exports.supports = require('./src/supports')
 exports.resolveSupport = require('./src/resolveSupport')
-exports.isBroadSignPlayer = require('./src/isBroadSignPlayer')
 exports.BroadSignActions = require('./src/Broadsignlink')
+
+exports.isBroadSignPlayer = require('./src/isBroadSignPlayer')
+exports.BroadSignData = require('./src/BroadSignData')
 
 exports.cache = require('./src/cache')

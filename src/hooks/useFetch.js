b517968085806ee195c6aa6e16e4d5fcb800df5d
valentin-dufois <19367658+valentin-dufois@users.noreply.github.com>
2021-06-14 15:53:53 -0400

    WiP 2021-06-14

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import useCache from './useCache'
+import React          from 'react';
 import DynamicContext from '../components/DynamicContext';
+import useCache       from './useCache';
 
 /**
  *
@@ -11,27 +11,28 @@ import DynamicContext from '../components/DynamicContext';
  */
 const useFetch = (url, method = 'get', body = null, auth = false) => {
   const { api } = React.useContext(DynamicContext);
-  let request;
+  const request = React.useMemo(() => {
+    if (typeof url === 'string' || url instanceof URL) {
+      return api.prepareUrl(url, method, body, auth);
+    }
 
-  if(typeof url === 'string' || url instanceof URL) {
-    request = api.prepareUrl(url, method, body, auth)
-  } else {
-    request = url;
-  }
+    return url;
+  }, [ url, method, body, auth ]);
 
-  const cache = useCache();
+
+  const cache                       = useCache();
   const [ isLoading, setIsLoading ] = React.useState(true);
-  const [ response, setResponse ] = React.useState(null);
+  const [ response, setResponse ]   = React.useState(null);
 
   React.useEffect(() => {
     cache.get(request.url, () => fetch(request))
-      .then(response => {
-        setResponse(response)
-        setIsLoading(false)
-      })
-  }, [request])
+         .then(response => {
+           setResponse(response);
+           setIsLoading(false);
+         });
+  }, [ request ]);
 
-  return [response, isLoading];
+  return [ response, isLoading ];
 };
 
 export default useFetch;

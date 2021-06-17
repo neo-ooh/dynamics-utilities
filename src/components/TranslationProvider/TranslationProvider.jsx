6d2ca1f39cad3759495fae4c909e1a14814b75a9
valentin-dufois <19367658+valentin-dufois@users.noreply.github.com>
2021-06-17 15:04:54 -0400

    WiP 2021-06-17

--- src/components/TranslationProvider/TranslationProvider.jsx
+++ src/components/TranslationProvider/TranslationProvider.jsx
@@ -22,6 +22,7 @@ const TranslationProvider = ({ children, locales }) => {
 
   React.useEffect(() => {
     for (const locale in Object.keys(locales)) {
+      console.log(locale)
       for (const namespace in Object.keys(locales[locale])) {
         i18n.addResourceBundle(locale, namespace, locales[locale][namespace]);
       }
